0c4c2202d94630456725a992225adfd216e6c340
pablo <pawkli95@gmail.com>
2021-08-31 15:35:54 +0200

    Task2 solution

---
+++ app/src/main/java/pl/futurecollars/invoicing/model/InvoiceEntry.java
@@ -0,0 +1,20 @@
+package pl.futurecollars.invoicing.model;
+
+import java.math.BigDecimal;
+import lombok.Data;
+
+@Data
+public class InvoiceEntry {
+
+    private final String description;
+    private final BigDecimal price;
+    private final Vat vatRate;
+    private final BigDecimal vatValue;
+
+    public InvoiceEntry(String description, BigDecimal price, Vat vatRate) {
+        this.description = description;
+        this.price = price;
+        this.vatRate = vatRate;
+        this.vatValue = price.multiply(BigDecimal.valueOf(vatRate.rate));
+    }
+}
