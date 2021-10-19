ad799fcaf58d5141efa1e6f17c7d09f7c7b67f5a
pawkli95 <81581334+pawkli95@users.noreply.github.com>
2021-10-19 16:42:39 +0200

    Task11 (#12)

---
+++ app/src/main/resources/db/migration/V2__Create_companies_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE companies
+(
+tax_id varchar(10) NOT NULL,
+company_name varchar(50) NOT NULL,
+address varchar(100) NOT NULL,
+health_insurance numeric(50, 2) NOT NULL,
+pension_insurance numeric(50, 2) NOT NULL,
+PRIMARY KEY (tax_id)
+);
+
